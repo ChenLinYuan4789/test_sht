2e765f17d2f768beca5a410e04e54b7ba5d95a1c
ChenLinYuan4789 <70091643+ChenLinYuan4789@users.noreply.github.com>
2020-08-27 13:48:51 +0800

    更新输出的文本描述

--- script/test_contact.py
+++ script/test_contact.py
@@ -8,7 +8,7 @@ class TestContact():
     @allure.step(title="登录的测试脚本")
     def test_login(self):
         allure.attach("输入用户名", "测试输入了:hello, 1, str")
-        print("输入用户名")
+        print("输入你的用户名")
         allure.attach("输入密码", "输入密码的描述")
         print("输入密码")
         allure.attach("登录", "登录的描述")
